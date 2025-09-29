49831d17cc76c038f5f176726e08a650981b854e
lhyuanmhd <lh.yuan.mhd@gmail.com>
2025-09-29 14:53:08 +0200

    complete work flow

--- src/quicc_dynavis/fields_snapshot.py
+++ src/quicc_dynavis/fields_snapshot.py
@@ -1,11 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def load_fields(filename):
-    """Load fields and coordinates from npz file."""
-    data = np.load(filename)
-    return data
-
 field_latex = {
     "u_r": r"u_r",
     "u_theta": r"u_\theta",
@@ -16,7 +11,8 @@ field_latex = {
     "temperature": r"T"
 }
 
-def plot_equatorial(data, field_name, r, phi, theta, title="Equatorial slice", ax=None):
+def plot_equatorial(data, field_name, title="Equatorial slice", cmap="RdBu_r", ax=None, savefig=None):
+    r, theta, phi = data["r"], data["theta"], data["phi"] 
     eq_idx = np.argmin(np.abs(theta - np.pi/2))
     field = data[field_name][:, eq_idx, :]
 
@@ -25,17 +21,22 @@ def plot_equatorial(data, field_name, r, phi, theta, title="Equatorial slice", a
 
     if ax is None:
         fig, ax = plt.subplots(figsize=(6,6))
-    im = ax.pcolormesh(X, Y, field, shading="auto", cmap="RdBu_r")
+    im = ax.pcolormesh(X, Y, field, shading="auto", cmap=cmap)
     ax.set_aspect("equal")
     ax.axis("off")
     label_str = field_latex.get(field_name, field_name)
     plt.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
-    ax.set_title(rf"${label_str}$", pad=10)
+    ax.set_title(rf"${label_str}$", pad=10, fontsize=16)
 
-def plot_meridional(data, field_name, r, phi, theta, title="Meridional slice", ax=None):
+    if savefig is not None:
+        plt.savefig(savefig, dpi=300, bbox_inches="tight")
+
+
+def plot_meridional(data, field_name, title="Meridional slice", cmap="RdBu_r", ax=None, savefig=None):
     """
     Plot a meridional slice (half-circle) of the field at mid-phi.
     """
+    r, theta, phi = data["r"], data["theta"], data["phi"] 
     mid_phi = np.argmin(np.abs(phi - np.pi/2)) #phi=pi/2
     field = data[field_name][:, :, mid_phi]  # shape (len(r), len(theta))
 
@@ -44,7 +45,7 @@ def plot_meridional(data, field_name, r, phi, theta, title="Meridional slice", a
 
     if ax is None:
         fig, ax = plt.subplots(figsize=(6,6))
-    im = ax.pcolormesh(X, Z, field, shading="auto", cmap="RdBu_r")
+    im = ax.pcolormesh(X, Z, field, shading="auto", cmap=cmap)
     ax.set_aspect("equal")
     ax.axis("off")
 
@@ -52,10 +53,15 @@ def plot_meridional(data, field_name, r, phi, theta, title="Meridional slice", a
     label_str = field_latex.get(field_name, field_name)
 
     plt.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
-    ax.set_title(rf"${label_str}$", pad=10)
+    ax.set_title(rf"${label_str}$", pad=10, fontsize=16)
+
+    if savefig is not None:
+        plt.savefig(savefig, dpi=300, bbox_inches="tight")
 
 
-def plot_cmb(data, field_name, r, phi, theta, title="CMB", ax=None):
+
+def plot_cmb(data, field_name, title="CMB", cmap="RdBu_r", ax=None, savefig=None):
+    r, theta, phi = data["r"], data["theta"], data["phi"] 
     field = data[field_name][-1, :, :]
     lon, lat = phi - np.pi, np.pi/2 - theta
     Lon, Lat = np.meshgrid(lon, lat, indexing="ij")
@@ -63,8 +69,11 @@ def plot_cmb(data, field_name, r, phi, theta, title="CMB", ax=None):
     if ax is None:
         fig = plt.figure(figsize=(8,5))
         ax = fig.add_subplot(111, projection="mollweide")
-    im = ax.pcolormesh(Lon, Lat, field.T, shading="auto", cmap="RdBu_r")
+    im = ax.pcolormesh(Lon, Lat, field.T, shading="auto", cmap=cmap)
     ax.set_axis_off()
     label_str = field_latex.get(field_name, field_name)
     plt.colorbar(im, ax=ax, orientation="horizontal", pad=0.05, fraction=0.05)
+    ax.set_title(rf"${label_str}$", pad=10, fontsize=16)
 
+    if savefig is not None:
+        plt.savefig(savefig, dpi=300, bbox_inches="tight")
