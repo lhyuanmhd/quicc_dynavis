ed85197d403bdad1796f73d55b06690aa79389ea
lhyuanmhd <lh.yuan.mhd@gmail.com>
2026-09-02 17:53:39 +0200

    fix the missing of factor of 0.5 in computaion of viscous dissipation lengthscale

--- src/quicc_dynavis/timeseries_diagnostics.py
+++ src/quicc_dynavis/timeseries_diagnostics.py
@@ -259,7 +259,7 @@ def compute_dynamo_diagnostics(
         vel_dis_length_scale = float(
             np.sqrt(
                   np.mean(data.kin_total[start_index:])
-                / 2*np.mean(data.kin_dis_total[start_index:]) # mising a factor of 2 before!
+                / 0.5*np.mean(data.kin_dis_total[start_index:]) # mising a factor of 0.5 before!
             )
         )
     else:
